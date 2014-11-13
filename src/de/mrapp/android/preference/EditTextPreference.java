2dbed0d485e8730ea5cc6efafe73e57f401779cb
mrapp <mrapp@localhost>
2014-11-13 20:38:13 +0100

    Added comments.

--- src/de/mrapp/android/preference/EditTextPreference.java
+++ src/de/mrapp/android/preference/EditTextPreference.java
@@ -1,3 +1,20 @@
+/*
+ * AndroidMaterialPreferences Copyright 2014 Michael Rapp
+ *
+ * This program is free software: you can redistribute it and/or modify 
+ * it under the terms of the GNU Lesser General Public License as published 
+ * by the Free Software Foundation, either version 3 of the License, or 
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful, but 
+ * WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU 
+ * General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public 
+ * License along with this program.  If not, see
+ * <http://www.gnu.org/licenses/>. 
+ */
 package de.mrapp.android.preference;
 
 import android.annotation.TargetApi;
@@ -12,82 +29,198 @@ import android.view.View;
 import android.widget.EditText;
 import de.mrapp.android.dialog.MaterialDialogBuilder;
 
+/**
+ * A preference, which allows to enter a text via an {@link EditText} widget.
+ * The entered text will only be persisted, if confirmed by the user.
+ * 
+ * @author Michael Rapp
+ *
+ * @since 1.0.0
+ */
 public class EditTextPreference extends AbstractDialogPreference {
 
+	/**
+	 * A data structure, which allows to save the internal state of an
+	 * {@link EditTextPreference}.
+	 */
 	public static class SavedState extends BaseSavedState {
 
+		/**
+		 * A creator, which allows to create instances of the class
+		 * {@link EditTextPreference} from parcels.
+		 */
 		public static final Parcelable.Creator<SavedState> CREATOR = new Parcelable.Creator<SavedState>() {
 
 			@Override
-			public SavedState createFromParcel(Parcel in) {
+			public SavedState createFromParcel(final Parcel in) {
 				return new SavedState(in);
 			}
 
 			@Override
-			public SavedState[] newArray(int size) {
+			public SavedState[] newArray(final int size) {
 				return new SavedState[size];
 			}
 
 		};
 
+		/**
+		 * The saved value of the attribute "text".
+		 */
 		public String text;
 
-		public SavedState(Parcel source) {
+		/**
+		 * Creates a new data structure, which allows to store the internal
+		 * state of an {@link EditTextPreference}. This constructor is called by
+		 * derived classes when saving their states.
+		 * 
+		 * @param superState
+		 *            The state of the superclass of this view, as an instance
+		 *            of the type {@link Parcelable}
+		 */
+		public SavedState(final Parcelable superState) {
+			super(superState);
+		}
+
+		/**
+		 * Creates a new data structure, which allows to store the internal
+		 * state of an {@link EditTextPreference}. This constructor is used when
+		 * reading from a parcel. It reads the state of the superclass.
+		 * 
+		 * @param source
+		 *            The parcel to read read from as a instance of the class
+		 *            {@link Parcel}
+		 */
+		public SavedState(final Parcel source) {
 			super(source);
 			text = source.readString();
 		}
 
-		public SavedState(Parcelable superState) {
-			super(superState);
-		}
-
 		@Override
-		public void writeToParcel(Parcel dest, int flags) {
+		public final void writeToParcel(final Parcel dest, final int flags) {
 			super.writeToParcel(dest, flags);
 			dest.writeString(text);
 		}
 
 	};
 
+	/**
+	 * The {@link EditText} widget, which allows the user to enter a text.
+	 */
 	private EditText editText;
 
+	/**
+	 * The currently persisted text.
+	 */
 	private String text;
 
+	/**
+	 * Initializes the preference.
+	 */
 	private void initialize() {
 		setPositiveButtonText(android.R.string.ok);
 		setNegativeButtonText(android.R.string.cancel);
 	}
 
+	/**
+	 * Creates a new preference, which allows to enter a text via an
+	 * {@link EditText} widget.
+	 * 
+	 * @param context
+	 *            The context, which should be used by the preference, as an
+	 *            instance of the class {@link Context}
+	 */
 	public EditTextPreference(final Context context) {
 		super(context);
 		initialize();
 	}
 
+	/**
+	 * Creates a new preference, which allows to enter a text via an
+	 * {@link EditText} widget.
+	 * 
+	 * @param context
+	 *            The context, which should be used by the preference, as an
+	 *            instance of the class {@link Context}
+	 * @param attributeSet
+	 *            The attributes of the XML tag that is inflating the
+	 *            preference, as an instance of the type {@link AttributeSet}
+	 */
 	public EditTextPreference(final Context context,
 			final AttributeSet attributeSet) {
 		super(context, attributeSet);
 		initialize();
 	}
 
+	/**
+	 * Creates a new preference, which allows to enter a text via an
+	 * {@link EditText} widget.
+	 * 
+	 * @param context
+	 *            The context, which should be used by the preference, as an
+	 *            instance of the class {@link Context}
+	 * @param attributeSet
+	 *            The attributes of the XML tag that is inflating the
+	 *            preference, as an instance of the type {@link AttributeSet}
+	 * @param defaultStyle
+	 *            The default style to apply to this preference. If 0, no style
+	 *            will be applied (beyond what is included in the theme). This
+	 *            may either be an attribute resource, whose value will be
+	 *            retrieved from the current theme, or an explicit style
+	 *            resource
+	 */
 	public EditTextPreference(final Context context,
-			final AttributeSet attributeSet, final int defStyleAttr) {
-		super(context, attributeSet, defStyleAttr);
+			final AttributeSet attributeSet, final int defaultStyle) {
+		super(context, attributeSet, defaultStyle);
 		initialize();
 	}
 
+	/**
+	 * Creates a new preference, which allows to enter a text via an
+	 * {@link EditText} widget.
+	 * 
+	 * @param context
+	 *            The context, which should be used by the preference, as an
+	 *            instance of the class {@link Context}
+	 * @param attributeSet
+	 *            The attributes of the XML tag that is inflating the
+	 *            preference, as an instance of the type {@link AttributeSet}
+	 * @param defaultStyle
+	 *            The default style to apply to this preference. If 0, no style
+	 *            will be applied (beyond what is included in the theme). This
+	 *            may either be an attribute resource, whose value will be
+	 *            retrieved from the current theme, or an explicit style
+	 *            resource
+	 * @param defaultStyleResource
+	 *            A resource identifier of a style resource that supplies
+	 *            default values for the preference, used only if the default
+	 *            style is 0 or can not be found in the theme. Can be 0 to not
+	 *            look for defaults
+	 */
 	@TargetApi(Build.VERSION_CODES.LOLLIPOP)
 	public EditTextPreference(final Context context,
-			final AttributeSet attributeSet, final int defStyleAttr,
-			final int defStyleRes) {
-		super(context, attributeSet, defStyleAttr, defStyleRes);
+			final AttributeSet attributeSet, final int defaultStyle,
+			final int defaultStyleResource) {
+		super(context, attributeSet, defaultStyle, defaultStyleResource);
 		initialize();
 	}
 
-	public String getText() {
+	/**
+	 * Returns the currently persisted text of the preference.
+	 * 
+	 * @return The currently persisted text as a {@link String}
+	 */
+	public final String getText() {
 		return text;
 	}
 
-	public void setText(String text) {
+	/**
+	 * Sets the current text of the preference. By setting a value, it will be
+	 * persisted.
+	 * 
+	 * @param text
+	 *            The text, which should be set, as a {@link String}
+	 */
+	public final void setText(final String text) {
 		boolean hasDisabledDependents = shouldDisableDependents();
 		this.text = text;
 		persistString(text);
@@ -99,12 +232,13 @@ public class EditTextPreference extends AbstractDialogPreference {
 	}
 
 	@Override
-	public boolean shouldDisableDependents() {
+	public final boolean shouldDisableDependents() {
 		return TextUtils.isEmpty(getText()) || super.shouldDisableDependents();
 	}
 
 	@Override
-	protected void onPrepareDialog(MaterialDialogBuilder dialogBuilder) {
+	protected final void onPrepareDialog(
+			final MaterialDialogBuilder dialogBuilder) {
 		editText = (EditText) View.inflate(getContext(), R.layout.edit_text,
 				null);
 		editText.setText(getText());
@@ -112,7 +246,7 @@ public class EditTextPreference extends AbstractDialogPreference {
 	}
 
 	@Override
-	protected void onDialogClosed(boolean positiveResult) {
+	protected final void onDialogClosed(final boolean positiveResult) {
 		if (positiveResult) {
 			String newValue = editText.getText().toString();
 
@@ -125,23 +259,25 @@ public class EditTextPreference extends AbstractDialogPreference {
 	}
 
 	@Override
-	protected boolean needInputMethod() {
+	protected final boolean needInputMethod() {
 		return true;
 	}
 
 	@Override
-	protected Object onGetDefaultValue(TypedArray typedArray, int index) {
+	protected final Object onGetDefaultValue(final TypedArray typedArray,
+			final int index) {
 		return typedArray.getString(index);
 	}
 
 	@Override
-	protected void onSetInitialValue(boolean restoreValue, Object defaultValue) {
+	protected final void onSetInitialValue(final boolean restoreValue,
+			final Object defaultValue) {
 		setText(restoreValue ? getPersistedString(getText())
 				: (String) defaultValue);
 	}
 
 	@Override
-	protected Parcelable onSaveInstanceState() {
+	protected final Parcelable onSaveInstanceState() {
 		Parcelable parcelable = super.onSaveInstanceState();
 
 		if (!isPersistent()) {
@@ -154,7 +290,7 @@ public class EditTextPreference extends AbstractDialogPreference {
 	}
 
 	@Override
-	protected void onRestoreInstanceState(Parcelable state) {
+	protected final void onRestoreInstanceState(final Parcelable state) {
 		if (state != null && state instanceof SavedState) {
 			SavedState savedState = (SavedState) state;
 			setText(savedState.text);
