ef5c9ba75f3edcc5abbee1b7a2db7d6bd6898c1f
Michael Rapp <michael.rapp90@googlemail.com>
2015-09-15 22:22:03 +0200

    Added the class AbstractListPreference.

--- src/de/mrapp/android/preference/ListPreference.java
+++ src/de/mrapp/android/preference/ListPreference.java
@@ -28,7 +28,6 @@ import android.os.Parcelable;
 import android.text.TextUtils;
 import android.util.AttributeSet;
 import de.mrapp.android.dialog.MaterialDialogBuilder;
-import static de.mrapp.android.preference.util.Condition.ensureNotNull;
 
 /**
  * A preference, which allows to select a value from a list. The selected value
@@ -38,7 +37,7 @@ import static de.mrapp.android.preference.util.Condition.ensureNotNull;
  *
  * @since 1.0.0
  */
-public class ListPreference extends AbstractDialogPreference {
+public class ListPreference extends AbstractListPreference {
 
 	/**
 	 * A data structure, which allows to save the internal state of an
@@ -104,27 +103,6 @@ public class ListPreference extends AbstractDialogPreference {
 	};
 
 	/**
-	 * The color of the items of the preference's dialog.
-	 */
-	private int dialogItemColor;
-
-	/**
-	 * The color of the item controls of the preference's dialog.
-	 */
-	private int dialogItemControlColor;
-
-	/**
-	 * An array, which contains the entries, which are shown in the list.
-	 */
-	private CharSequence[] entries;
-
-	/**
-	 * An array, which contains the values, which correspond to the entries,
-	 * which are shown in the list.
-	 */
-	private CharSequence[] entryValues;
-
-	/**
 	 * The currently persisted value of the preference.
 	 */
 	private String value;
@@ -136,92 +114,9 @@ public class ListPreference extends AbstractDialogPreference {
 
 	/**
 	 * Initializes the list preference.
-	 * 
-	 * @param attributeSet
-	 *            The attribute set, which should be used to initialize the list
-	 *            preferences, as an instance of the type {@link AttributeSet}
 	 */
-	private void initialize(final AttributeSet attributeSet) {
-		entries = new CharSequence[0];
-		entryValues = new CharSequence[0];
+	private void initialize() {
 		setNegativeButtonText(android.R.string.cancel);
-		obtainStyledAttributes(attributeSet);
-	}
-
-	/**
-	 * Obtains all attributes from a specific attribute set.
-	 * 
-	 * @param attributeSet
-	 *            The attribute set, the attributes should be obtained from, as
-	 *            an instance of the type {@link AttributeSet}
-	 */
-	private void obtainStyledAttributes(final AttributeSet attributeSet) {
-		TypedArray typedArray = getContext().obtainStyledAttributes(attributeSet, R.styleable.ListPreference);
-
-		try {
-			obtainDialogItemColor(typedArray);
-			obtainDialogItemControlColor(typedArray);
-			obtainEntries(typedArray);
-			obtainEntryValues(typedArray);
-		} finally {
-			typedArray.recycle();
-		}
-	}
-
-	/**
-	 * Obtains the item color of the preference's dialog from a specific typed
-	 * array.
-	 * 
-	 * @param typedArray
-	 *            The typed array, the item color should be obtained from, as an
-	 *            instance of the class {@link TypedArray}
-	 */
-	private void obtainDialogItemColor(final TypedArray typedArray) {
-		setDialogItemColor(typedArray.getColor(R.styleable.ListPreference_dialogItemColor, -1));
-	}
-
-	/**
-	 * Obtains the item control color of the preference's dialog from a specific
-	 * typed array.
-	 * 
-	 * @param typedArray
-	 *            The typed array, the item control color should be obtained
-	 *            from, as an instance of the class {@link TypedArray}
-	 */
-	private void obtainDialogItemControlColor(final TypedArray typedArray) {
-		setDialogItemControlColor(typedArray.getColor(R.styleable.ListPreference_dialogItemControlColor, -1));
-	}
-
-	/**
-	 * Obtains the the entries of the list preference from a specific typed
-	 * array.
-	 * 
-	 * @param typedArray
-	 *            The typed array, the entries should be obtained from, as an
-	 *            instance of the class {@link TypedArray}
-	 */
-	private void obtainEntries(final TypedArray typedArray) {
-		CharSequence[] obtainedEntries = typedArray.getTextArray(R.styleable.ListPreference_android_entries);
-
-		if (obtainedEntries != null) {
-			setEntries(obtainedEntries);
-		}
-	}
-
-	/**
-	 * Obtains the the values, which correspond to the entries of the list
-	 * preference from a specific typed array.
-	 * 
-	 * @param typedArray
-	 *            The typed array, the entry values should be obtained from, as
-	 *            an instance of the class {@link TypedArray}
-	 */
-	private void obtainEntryValues(final TypedArray typedArray) {
-		CharSequence[] obtainedEntryValues = typedArray.getTextArray(R.styleable.ListPreference_android_entryValues);
-
-		if (obtainedEntryValues != null) {
-			setEntryValues(obtainedEntryValues);
-		}
 	}
 
 	/**
@@ -234,9 +129,9 @@ public class ListPreference extends AbstractDialogPreference {
 	 *         value or -1 if the list does not contain such a value
 	 */
 	private int indexOf(final CharSequence value) {
-		if (value != null && entryValues != null) {
-			for (int i = entryValues.length - 1; i >= 0; i--) {
-				if (entryValues[i].equals(value)) {
+		if (value != null && getEntryValues() != null) {
+			for (int i = getEntryValues().length - 1; i >= 0; i--) {
+				if (getEntryValues()[i].equals(value)) {
 					return i;
 				}
 			}
@@ -273,7 +168,7 @@ public class ListPreference extends AbstractDialogPreference {
 	 */
 	public ListPreference(final Context context) {
 		super(context);
-		initialize(null);
+		initialize();
 	}
 
 	/**
@@ -288,7 +183,7 @@ public class ListPreference extends AbstractDialogPreference {
 	 */
 	public ListPreference(final Context context, final AttributeSet attributeSet) {
 		super(context, attributeSet);
-		initialize(attributeSet);
+		initialize();
 	}
 
 	/**
@@ -309,7 +204,7 @@ public class ListPreference extends AbstractDialogPreference {
 	 */
 	public ListPreference(final Context context, final AttributeSet attributeSet, final int defaultStyle) {
 		super(context, attributeSet, defaultStyle);
-		initialize(attributeSet);
+		initialize();
 	}
 
 	/**
@@ -337,123 +232,7 @@ public class ListPreference extends AbstractDialogPreference {
 	public ListPreference(final Context context, final AttributeSet attributeSet, final int defaultStyle,
 			final int defaultStyleResource) {
 		super(context, attributeSet, defaultStyle, defaultStyleResource);
-		initialize(attributeSet);
-	}
-
-	/**
-	 * Returns the color of the items of the preference's dialog.
-	 * 
-	 * @return The color of the items as an {@link Integer} value or -1, if no
-	 *         custom item color is set
-	 */
-	public final int getDialogItemColor() {
-		return dialogItemColor;
-	}
-
-	/**
-	 * Sets the color of the items of the preference's dialog.
-	 * 
-	 * @param color
-	 *            The color, which should be set, as an {@link Integer} value or
-	 *            -1, if no custom item color should be set
-	 */
-	public final void setDialogItemColor(final int color) {
-		this.dialogItemColor = color;
-	}
-
-	/**
-	 * Returns the color of the item controls of the preference's dialog.
-	 * 
-	 * @return The color of the item controls as an {@link Integer} value or -1,
-	 *         if no custom item color is set
-	 */
-	public final int getDialogItemControlColor() {
-		return dialogItemControlColor;
-	}
-
-	/**
-	 * Sets the color of the item controls of the preference's dialog.
-	 * 
-	 * @param color
-	 *            The color, which should be set, as an {@link Integer} value or
-	 *            -1, if no custom item control color should be set
-	 */
-	@TargetApi(Build.VERSION_CODES.JELLY_BEAN)
-	public final void setDialogItemControlColor(final int color) {
-		this.dialogItemControlColor = color;
-	}
-
-	/**
-	 * Returns the entries of the list, which is shown by the preference.
-	 * 
-	 * @return The entries of the list, which is shown by the preference, as a
-	 *         {@link CharSequence} array or null, if no entries have been set
-	 */
-	public final CharSequence[] getEntries() {
-		return entries;
-	}
-
-	/**
-	 * Sets the entries of the list, which is shown by the preference.
-	 * 
-	 * @param entries
-	 *            The entries, which should be set, as an {@link CharSequence}
-	 *            array. The entries may not be null
-	 */
-	public final void setEntries(final CharSequence[] entries) {
-		ensureNotNull(entries, "The entries may not be null");
-		this.entries = entries;
-	}
-
-	/**
-	 * Sets the entries of the list, which is shown by the preference.
-	 * 
-	 * @param resourceId
-	 *            The resource id of the entries, which should be set, as an
-	 *            {@link Integer} value. The resource id must correspond to a
-	 *            valid array resource
-	 */
-	public final void setEntries(final int resourceId) {
-		setEntries(getContext().getResources().getTextArray(resourceId));
-	}
-
-	/**
-	 * Returns the values, which correspond to the entries of the list, which is
-	 * shown by the preference.
-	 * 
-	 * @return The values, which correspond to the entries of the list, which is
-	 *         shown by the preference, as a {@link CharSequence} array
-	 */
-	public final CharSequence[] getEntryValues() {
-		return entryValues;
-	}
-
-	/**
-	 * Sets the values, which correspond to the entries of the list, which is
-	 * shown by the preference.
-	 * 
-	 * @param entryValues
-	 *            The values, which should be set, as a {@link CharSequence}
-	 *            array. The values may not be null and the array's length must
-	 *            be equal to the number of list items
-	 */
-	public final void setEntryValues(final CharSequence[] entryValues) {
-		ensureNotNull(entryValues, "The entry values may not be null");
-		this.entryValues = entryValues;
-	}
-
-	/**
-	 * Sets the values, which correspond to the entries of the list, which is
-	 * shown by the preference.
-	 * 
-	 * @param resourceId
-	 *            The resource id of the values, which should be set, as an
-	 *            {@link Integer} value. The resource id must correspond to a
-	 *            valid array resource. The array's length must be equal to the
-	 *            number of list items
-	 */
-	public final void setEntryValues(final int resourceId) {
-		setEntryValues(getContext().getResources().getTextArray(resourceId));
+		initialize();
 	}
 
 	/**
@@ -490,8 +269,8 @@ public class ListPreference extends AbstractDialogPreference {
 	 *            {@link Integer} value
 	 */
 	public final void setValueIndex(final int index) {
-		if (entryValues != null) {
-			setValue(entryValues[index].toString());
+		if (getEntryValues() != null) {
+			setValue(getEntryValues()[index].toString());
 		}
 	}
 
@@ -504,7 +283,7 @@ public class ListPreference extends AbstractDialogPreference {
 	 */
 	public final CharSequence getEntry() {
 		int index = indexOf(value);
-		return index >= 0 && entries != null ? entries[index] : null;
+		return index >= 0 && getEntries() != null ? getEntries()[index] : null;
 	}
 
 	@Override
@@ -517,8 +296,8 @@ public class ListPreference extends AbstractDialogPreference {
 	}
 
 	@Override
-	protected final Object onGetDefaultValue(final TypedArray a, final int index) {
-		return a.getString(index);
+	protected final Object onGetDefaultValue(final TypedArray typedArray, final int index) {
+		return typedArray.getString(index);
 	}
 
 	@Override
@@ -527,22 +306,17 @@ public class ListPreference extends AbstractDialogPreference {
 	}
 
 	@Override
-	protected final boolean needInputMethod() {
-		return false;
-	}
-
-	@Override
 	protected final void onPrepareDialog(final MaterialDialogBuilder dialogBuilder) {
 		selectedIndex = indexOf(getValue());
-		dialogBuilder.setSingleChoiceItems(entries, selectedIndex, createListItemListener());
+		dialogBuilder.setSingleChoiceItems(getEntries(), selectedIndex, createListItemListener());
 		dialogBuilder.setItemColor(getDialogItemColor());
 		dialogBuilder.setItemControlColor(getDialogItemColor());
 	}
 
 	@Override
 	protected final void onDialogClosed(final boolean positiveResult) {
-		if (positiveResult && selectedIndex >= 0 && entryValues != null) {
-			String newValue = entryValues[selectedIndex].toString();
+		if (positiveResult && selectedIndex >= 0 && getEntryValues() != null) {
+			String newValue = getEntryValues()[selectedIndex].toString();
 
 			if (callChangeListener(newValue)) {
 				setValue(newValue);
