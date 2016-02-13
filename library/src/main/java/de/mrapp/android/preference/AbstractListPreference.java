f8edfee891e0cd06c7ad4ef86190cd9ee90c17bd
Michael Rapp <michael.rapp90@googlemail.com>
2016-02-13 01:25:14 +0100

    Updated copyright information.

--- library/src/main/java/de/mrapp/android/preference/AbstractListPreference.java
+++ library/src/main/java/de/mrapp/android/preference/AbstractListPreference.java
@@ -1,5 +1,5 @@
 /*
- * AndroidMaterialPreferences Copyright 2014 - 2015 Michael Rapp
+ * AndroidMaterialPreferences Copyright 2014 - 2016 Michael Rapp
  *
  * This program is free software: you can redistribute it and/or modify it under the terms of the
  * GNU Lesser General Public License as published by the Free Software Foundation, either version 3
